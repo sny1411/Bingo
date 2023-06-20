47c67aa9e2300f30c1892e49faad5b6664e2fbda
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-06-20 18:36:16 +0200

    change messages join and quit players

--- src/main/java/fr/sny1411/bingo/listener/PlayerListener.java
+++ src/main/java/fr/sny1411/bingo/listener/PlayerListener.java
@@ -3,19 +3,27 @@ package fr.sny1411.bingo.listener;
 import fr.sny1411.bingo.Bingo;
 import fr.sny1411.bingo.Game;
 import fr.sny1411.bingo.utils.Spawn;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent e) {
+        e.joinMessage(Component.text(String.format("§8[§a+§8]§e %s", e.getPlayer())));
         if (Bingo.getGame().getEtat() == Game.Etat.SETUP) {
             Spawn.teleportPlayer(e.getPlayer());
         }
+    }
+
+    @EventHandler
+    private void onPlayerQuit(PlayerQuitEvent e) {
+        e.quitMessage(Component.text(String.format("§8[§c-§8]§e %s", e.getPlayer())));
     }
 
     @EventHandler
