eff36ffa154650f808e7d34dc90b30e7bca3fbae
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-06-20 12:15:57 +0200

    complete code Spawn.java

--- src/main/java/fr/sny1411/bingo/Bingo.java
+++ src/main/java/fr/sny1411/bingo/Bingo.java
@@ -1,13 +1,13 @@
 package fr.sny1411.bingo;
 
+import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bingo extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        PaperLib.suggestPaper(this);
     }
 
     @Override
--- src/main/java/fr/sny1411/bingo/utils/Spawn.java
+++ src/main/java/fr/sny1411/bingo/utils/Spawn.java
@@ -1,11 +1,19 @@
 package fr.sny1411.bingo.utils;
 
+import io.papermc.lib.PaperLib;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.logging.Level;
 
 public class Spawn {
-    private static final World overworld = Bukkit.getWorlds().get(0);
+    private static final List<World> worlds = Bukkit.getWorlds();
+    private static final World overworld = worlds.get(0);
+    private static final Location spawn = new Location(overworld, 0, 204, 0);
 
     public static void create() {
         BlocksFill.changeArea(-20, 200, -20, 20, 200, 20, Material.WHITE_STAINED_GLASS, overworld);
@@ -18,5 +26,21 @@ public class Spawn {
 
     public static void remove() {
         BlocksFill.changeArea(-20, 200, -20, 20, 203, 20, Material.AIR, overworld);
+    }
+
+    public static void teleportPlayers() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            teleportPlayer(player);
+        }
+    }
+
+    public static void teleportPlayer(Player player) {
+        PaperLib.teleportAsync(player, spawn).thenAccept(result -> {
+            if (Boolean.TRUE.equals(result)) {
+                Bukkit.getLogger().log(Level.INFO, String.format("%s teleport to spawn", player.getName()));
+            } else {
+                Bukkit.getLogger().log(Level.WARNING, String.format("%s teleport error", player.getName()));
+            }
+        });
     }
 }
