22c4d81ac416dcb1bf28ca40c3682e814bb81c75
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-06-20 11:26:04 +0200

    Environnement.java

---
+++ src/main/java/fr/sny1411/bingo/utils/Environnement.java
@@ -0,0 +1,22 @@
+package fr.sny1411.bingo.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Environnement {
+
+    private static final List<World> worlds = Bukkit.getWorlds();
+
+    public void setGamerule() {
+        for (World world : worlds) {
+            Objects.requireNonNull(Bukkit.getWorld(world.getName())).setDifficulty(Difficulty.HARD);
+            world.setGameRule(GameRule.DO_WEATHER_CYCLE, true);
+            world.setGameRule(GameRule.SPECTATORS_GENERATE_CHUNKS, false);
+        }
+    }
+}
