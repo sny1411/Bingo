701a2ef97f52050be4bcd2ec92aa908a388164db
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-06-21 13:13:17 +0200

    spawn color move player

--- src/main/java/fr/sny1411/bingo/listener/SetupListener.java
+++ src/main/java/fr/sny1411/bingo/listener/SetupListener.java
@@ -2,6 +2,12 @@ package fr.sny1411.bingo.listener;
 
 import fr.sny1411.bingo.Bingo;
 import fr.sny1411.bingo.Game;
+import fr.sny1411.bingo.utils.Team;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -44,7 +50,37 @@ public class SetupListener implements Listener {
     @EventHandler
     private void onPlayerMove(PlayerMoveEvent e) {
         if (game.getEtat() == Game.Etat.SETUP) {
-            // TODO : quand team fait, color spawn
+            Player player = e.getPlayer();
+            Team team = Team.getTeam(player);
+            if (team != null) {
+                Location location = e.getPlayer().getLocation();
+                location.setY(200);
+                Block block = Bukkit.getWorlds().get(0).getBlockAt(location);
+                Material type = block.getType();
+                if (type == Material.AIR || type == Material.WHITE_STAINED_GLASS) {
+                    return;
+                }
+                switch (team.getColor()) {
+                    case BLEU:
+                        block.setType(Material.LIGHT_BLUE_STAINED_GLASS);
+                        break;
+                    case ROSE:
+                        block.setType(Material.PINK_STAINED_GLASS);
+                        break;
+                    case VERT:
+                        block.setType(Material.LIME_STAINED_GLASS);
+                        break;
+                    case ROUGE:
+                        block.setType(Material.RED_STAINED_GLASS);
+                        break;
+                    case ORANGE:
+                        block.setType(Material.ORANGE_STAINED_GLASS);
+                        break;
+                    case VIOLET:
+                        block.setType(Material.PURPLE_STAINED_GLASS);
+                        break;
+                }
+            }
         }
     }
 }
