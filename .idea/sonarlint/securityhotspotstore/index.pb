822958ccd98bb089096a834d6e0660fb22cccb6e
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-06-22 20:07:28 +0200

    Correction String to Component

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-_
-/src/main/java/fr/sny1411/bingo/utils/Items.java,b\6\b65f1d479f80cbc82d84c74dc40e909d4c19d96e
--- src/main/java/fr/sny1411/bingo/utils/Text.java
+++ src/main/java/fr/sny1411/bingo/utils/Text.java
@@ -1,11 +1,13 @@
 package fr.sny1411.bingo.utils;
 
+import net.kyori.adventure.text.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public interface Text {
-    static List<String> divideString(String str) {
-        List<String> dividedStrings = new ArrayList<>();
+    static List<Component> divideString(String str) {
+        List<Component> dividedStrings = new ArrayList<>();
 
         String[] words = str.split(" ");
         StringBuilder currentString = new StringBuilder();
@@ -14,13 +16,13 @@ public interface Text {
             if (currentString.length() + word.length() <= 30) {
                 currentString.append(word).append(" ");
             } else {
-                dividedStrings.add(currentString.toString().trim());
+                dividedStrings.add(Component.text(currentString.toString().trim()));
                 currentString = new StringBuilder(word).append(" ");
             }
         }
 
         if (currentString.length() > 0) {
-            dividedStrings.add(currentString.toString().trim());
+            dividedStrings.add(Component.text(currentString.toString().trim()));
         }
 
         return dividedStrings;
