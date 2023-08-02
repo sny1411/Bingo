4c1638ea9050eb01b333172c5b2d3f19e368a805
matteo_humez <matteo_humez@ens.univ-artois.fr>
2023-08-02 02:44:06 +0200

    ajout commande bonus

---
+++ src/main/java/fr/sny1411/bingo/commands/completer/BonusCompleter.java
@@ -0,0 +1,16 @@
+package fr.sny1411.bingo.commands.completer;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BonusCompleter implements TabCompleter {
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
